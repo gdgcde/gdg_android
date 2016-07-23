a4d0701e8b09d7347e861409722289fab8f0277a
kchomontiel <kchomontiei@gmail.com>
2016-07-23 17:54:30 -0400

    Estructura del Proyecto

---
+++ app/src/main/java/org/gdgcde/eventsapp/api/ApiConfig.java
@@ -0,0 +1,7 @@
+package org.gdgcde.eventsapp.api;
+
+/**
+ * Created by Carlos Montiel on 23/07/2016.
+ */
+public class ApiConfig {
+}
---
+++ app/src/main/java/org/gdgcde/eventsapp/ui/eventos/Testes.java
@@ -0,0 +1,7 @@
+package org.gdgcde.eventsapp.ui.eventos;
+
+/**
+ * Created by Carlos Montiel on 23/07/2016.
+ */
+public class Testes {
+}
